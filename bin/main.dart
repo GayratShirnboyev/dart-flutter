4e2ae032b295791345b27ba0d88f1fd9cbb7339f
G'ayrat <shirnboyevgayrat@gmail.com>
2022-11-08 11:25:04 +0500

    texfield

---
+++ bin/main.dart
@@ -0,0 +1,67 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController controller = TextEditingController(text: '0');
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Input'),
+                controller: controller,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('+'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(28.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('-'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(28.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('*'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(28.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('/'),
+                ),
+              )
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
+//         
